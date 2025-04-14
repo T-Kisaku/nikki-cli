b83169c4d46f6a3429f574f52679c2946f30cbe3
Takashi Kisaku <kisakutakashi0226@gmail.com>
2025-04-14 10:37:31 +0900

    add: exercise feature

--- src/commands/habit.ts
+++ src/commands/habit.ts
@@ -1,54 +1,83 @@
 import { Command } from "@cliffy/command";
-
 import { Select } from "@cliffy/prompt/select";
+import { Input } from "@cliffy/prompt/input";
+import {
+  db,
+  eq,
+  exerciseLogs,
+  exerciseTypes,
+  meditationLogs,
+  vMeditationLogs,
+} from "@db/client.ts";
+import { rootTui, tuiExit } from "@src/tui/index.ts";
 import renderHeatmap, { HeatmapElement } from "@src/tui/heatmap.ts";
 import renderCountup from "@src/tui/countup.ts";
-import { GlobalOptions } from "@src/main.ts";
-import { db, eq, meditationLogs, vMeditationLogs } from "@db/client.ts";
-import { rootTui, tuiExit } from "@src/tui/index.ts";
 
-export default new Command<GlobalOptions>()
-  .description("Create or update a journal entry with a template")
+export default new Command()
+  .description("Habit tracking")
   .arguments("[habit:string]")
   .action(async (_options, habitArg) => {
-    let search: string;
-    if (habitArg === undefined) {
-      search = await Select.prompt({
-        message: "Pick a habit",
-        options: [
-          { name: "Meditation", value: "Meditation" },
-          { name: "Exercise", value: "## Exercise" },
-        ],
-      });
-    } else if (["Meditation"].includes(habitArg)) {
-      search = habitArg;
-    } else {
-      // TODO: ERROR HANDLING
-      console.log("error handling");
-      Deno.exit(1);
+    const habit = await getHabitChoice(habitArg);
+    switch (habit) {
+      case "meditation":
+        return await handleMeditation();
+      case "exercise":
+        return await handleExercise();
     }
-    switch (search) {
-      case "Meditation":
-        {
-          const inserted = (await db.insert(meditationLogs).values({})
-            .returning())[0];
-          const logs = await db.select().from(vMeditationLogs);
-          const elements: HeatmapElement[] = logs.map((log) => ({
-            datetime: log.startedAt as Date,
-            level: log.durationSeconds ? 3 : 0,
-          }));
-          const tui = rootTui();
-          await renderHeatmap(tui, elements);
-          await renderCountup(tui, 10);
-
-          await db.update(meditationLogs)
-            .set({ endedAt: new Date() })
-            .where(eq(meditationLogs.id, inserted.id));
-          await tuiExit(tui);
-        }
-        break;
-      default:
-        // startHeatMapTui(search, journalDir);
-        break;
-    }
+    console.error("Invalid habit choice");
+    Deno.exit(1);
   });
+
+async function getHabitChoice(habitArg?: string): Promise<string> {
+  if (habitArg && ["meditation", "exercise"].includes(habitArg.toLowerCase())) {
+    return habitArg.toLowerCase();
+  }
+  return await Select.prompt({
+    message: "Pick a habit",
+    options: [
+      { name: "Meditation", value: "meditation" },
+      { name: "Exercise", value: "exercise" },
+    ],
+  });
+}
+
+async function handleMeditation() {
+  const inserted = (await db.insert(meditationLogs).values({}).returning())[0];
+  const logs = await db.select().from(vMeditationLogs);
+  const elements: HeatmapElement[] = logs.map((log) => ({
+    datetime: log.startedAt as Date,
+    level: log.durationSeconds ? 3 : 0,
+  }));
+  const tui = rootTui();
+  await renderHeatmap(tui, elements);
+  console.clear();
+  await renderCountup(tui, 10);
+  console.clear();
+  const memo = prompt("Give me review!! >");
+  await db.update(meditationLogs)
+    .set({ endedAt: new Date(), memo })
+    .where(eq(meditationLogs.id, inserted.id));
+  await tuiExit(tui);
+}
+async function handleExercise() {
+  const types = await db.select().from(exerciseTypes);
+  const exerciseName = await Input.prompt({
+    message: "Choose a exercise",
+    suggestions: types.map((t) => t.name),
+  });
+  const exerciseTypeId = types.find((t) => t.name === exerciseName)?.id;
+  if (!exerciseTypeId) {
+    console.error("Invalid exercise choice");
+    return;
+  }
+  await db.insert(exerciseLogs).values({ exerciseTypeId: exerciseTypeId });
+  const logs = await db.select().from(exerciseLogs);
+  const elements: HeatmapElement[] = logs.map((log) => ({
+    datetime: log.datetime,
+    level: log.intensity as HeatmapElement["level"] || 3,
+  }));
+  const tui = rootTui();
+  await renderHeatmap(tui, elements);
+  console.clear();
+  await tuiExit(tui);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,9 @@
-import { Command } from "@cliffy/command";
 import { CompletionsCommand } from "@cliffy/command/completions";
-import editCmd from "@src/commands/edit.ts";
 import getGitTagName from "@src/utils/getGitTagName.ts";
-// import { askCmd } from "./commands/ask.ts";
 import habitCmd from "@src/commands/habit.ts";
 
-export type GlobalOptions = {
-  journalDir: string;
-};
-
-await new Command()
+await habitCmd
   .name("nik")
   .version(await getGitTagName())
-  .description("Nik - A CLI journaling tool")
-  // TODO: make middleware to check if the folder exists or permission is fine
-  .globalEnv("NIK_JOURNAL_DIR=<path:string>", "Directory for journal entries", {
-    required: true,
-    prefix: "NIK_",
-  })
-  // .action(async ({ journalDir }) => {
-  //   const search: string = await Select.prompt({
-  //     message: "Pick a habit",
-  //     options: [
-  //       { name: "Meditation", value: "## Meditation" },
-  //       { name: "Exercise", value: "## Exercise" },
-  //     ],
-  //   });
-  //   if (search == "## Meditation") {
-  //     const journalFile = await JournalFile.create({
-  //       journalDir,
-  //       template: "default",
-  //     });
-  //     journalFile.append(search);
-  //     await startHeatMapTui(search, journalDir);
-  //     // countup((current) => {
-  //     //   journalFile.append(`time: ${current}s`);
-  //     // });
-  //   } else {
-  //     await startHeatMapTui(search, journalDir);
-  //   }
-  // })
-  .command("edit", editCmd)
-  .command("habit", habitCmd)
   .command("completions", new CompletionsCommand())
   .parse(Deno.args);
