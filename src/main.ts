f8f6909ec567f249e1e031a249fc2b0cbcdf83e5
Takashi Kisaku <kisakutakashi0226@gmail.com>
2025-04-21 18:01:30 +0900

    fix: bug caused by name

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import habitCmd from "@src/commands/habit.ts";
 import updateCmd from "@src/commands/update.ts";
 
 await new Command()
+  .name("nik")
   .version(await getGitTagName())
   .command("habit", habitCmd)
   .command("update", updateCmd)
